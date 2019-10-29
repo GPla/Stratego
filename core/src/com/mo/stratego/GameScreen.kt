69141ecec02697afd9676b9bef5180ea7a241c74
GPla <g-platz@hotmail.de>
2019-10-29 16:40:39 +0100

    Added GameScreen

---
+++ core/src/com/mo/stratego/GameScreen.kt
@@ -0,0 +1,26 @@
+package com.mo.stratego
+
+import com.badlogic.gdx.Screen
+
+class GameScreen : Screen {
+    override fun hide() {
+    }
+
+    override fun show() {
+    }
+
+    override fun render(delta: Float) {
+    }
+
+    override fun pause() {
+    }
+
+    override fun resume() {
+    }
+
+    override fun resize(width: Int, height: Int) {
+    }
+
+    override fun dispose() {
+    }
+}
---
+++ core/src/com/mo/stratego/StrategoGame.kt
@@ -0,0 +1,29 @@
+package com.mo.stratego
+
+import com.badlogic.gdx.Game
+
+class StrategoGame : Game(){
+    override fun create() {
+        setScreen(GameScreen())
+    }
+
+    override fun render() {
+        super.render()
+    }
+
+    override fun pause() {
+        super.pause()
+    }
+
+    override fun resume() {
+        super.resume()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        super.resize(width, height)
+    }
+
+    override fun dispose() {
+        super.dispose()
+    }
+}

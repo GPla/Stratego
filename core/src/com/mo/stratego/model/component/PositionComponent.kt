6576000294ee0e7ca52ff5caa268cccafe5cdd6b
GPla <g-platz@hotmail.de>
2019-11-03 22:28:14 +0100

    Update PositionComponent.kt
    changed doc

--- core/src/com/mo/stratego/model/component/PositionComponent.kt
+++ core/src/com/mo/stratego/model/component/PositionComponent.kt
@@ -2,12 +2,9 @@ package com.mo.stratego.model.component
 
 import com.badlogic.ashley.core.Component
 import com.badlogic.gdx.math.Vector2
-import com.mo.stratego.model.system.RenderSystem
 
 /**
  * A component that specifies the map position at which an entity
- * is rendered.
- * @property position Vector2 map position
- * @property z Int specifies the render order for the [RenderSystem].
+ * is rendered
  */
 class PositionComponent(var position: Vector2, var z: Int = 0) : Component

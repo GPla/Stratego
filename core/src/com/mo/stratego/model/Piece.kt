5c2cea4e53708bef5b34f258432402a8fa3a9bf6
GPla <g-platz@hotmail.de>
2019-11-03 22:36:01 +0100

    Update Piece.kt
    init with piececomponent

--- core/src/com/mo/stratego/model/Piece.kt
+++ core/src/com/mo/stratego/model/Piece.kt
@@ -1,11 +1,14 @@
 package com.mo.stratego.model
 
 import com.badlogic.ashley.core.Entity
+import com.mo.stratego.model.component.PieceComponent
 
 /**
  * Class that represents a playing piece
  * @property rank Rank of the playing piece
  */
 class Piece(val rank: Rank) : Entity() {
-
+    init {
+        add(PieceComponent(this))
+    }
 }

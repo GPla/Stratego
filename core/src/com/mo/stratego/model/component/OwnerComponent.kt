f6341380e97f7c676ac7669f39e8da24308e290e
GPla <g-platz@hotmail.de>
2019-11-03 22:40:31 +0100

    Create OwnerComponent.kt

---
+++ core/src/com/mo/stratego/model/component/OwnerComponent.kt
@@ -0,0 +1,6 @@
+package com.mo.stratego.model.component
+
+import com.badlogic.ashley.core.Component
+import com.mo.stratego.model.player.Player
+
+class OwnerComponent(var player: Player) : Component

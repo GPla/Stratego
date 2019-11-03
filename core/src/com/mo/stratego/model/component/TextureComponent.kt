e30cdf25f6986794dbfa980e0387b8620501e557
GPla <g-platz@hotmail.de>
2019-11-03 22:28:34 +0100

    Update TextureComponent.kt
    added origin

--- core/src/com/mo/stratego/model/component/TextureComponent.kt
+++ core/src/com/mo/stratego/model/component/TextureComponent.kt
@@ -4,6 +4,11 @@ import com.badlogic.ashley.core.Component
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.math.Vector2
 
-// inspired by https://www.gamedevelopment.blog/full-libgdx-game-tutorial-entities-ashley/
-class TextureComponent(var region: TextureRegion, var scale: Vector2 = Vector2(1.0f, 1.0f),
-                       var rotation: Float = 0f, var isHidden: Boolean = false) : Component
+/**
+ * Components for drawing a texture on the screen
+ */
+class TextureComponent(var region: TextureRegion,
+                       var scale: Vector2 = Vector2(1.0f, 1.0f),
+                       var origin: Vector2 = Vector2(0f, 0f),
+                       var rotation: Float = 0f,
+                       var isHidden: Boolean = false) : Component

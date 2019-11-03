0beeaff173ff6712198116957fa6f534d8ae6cb8
GPla <g-platz@hotmail.de>
2019-11-03 22:27:14 +0100

    Create DirectionComponent.kt

---
+++ core/src/com/mo/stratego/model/component/DirectionComponent.kt
@@ -0,0 +1,7 @@
+package com.mo.stratego.model.component
+
+import com.badlogic.ashley.core.Component
+import com.mo.stratego.model.Direction
+
+class DirectionComponent(var direction: Direction) : Component
+

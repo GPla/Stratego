6c2f5ec36c1d63c20d1b96e98eabe6fe367d75c9
GPla <g-platz@hotmail.de>
2019-11-03 20:06:09 +0100

    Create Piece.kt
    base class for playing pieces

---
+++ core/src/com/mo/stratego/model/Piece.kt
@@ -0,0 +1,11 @@
+package com.mo.stratego.model
+
+import com.badlogic.ashley.core.Entity
+
+/**
+ * Class that represents a playing piece
+ * @property rank Rank of the playing piece
+ */
+class Piece(val rank: Rank) : Entity() {
+
+}

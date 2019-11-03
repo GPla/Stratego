72ebdf48608859ee7782a7029fa3d65906820cca
GPla <g-platz@hotmail.de>
2019-11-03 20:00:27 +0100

    Package refactoring

--- core/src/com/mo/stratego/model/component/AnimationComponent.kt
+++ core/src/com/mo/stratego/model/component/AnimationComponent.kt
@@ -1,4 +1,4 @@
-package com.mo.stratego.components
+package com.mo.stratego.model.component
 
 import com.badlogic.ashley.core.Component
 
---
+++ core/src/com/mo/stratego/model/component/TextureComponent.kt
@@ -0,0 +1,9 @@
+package com.mo.stratego.model.component
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.Vector2
+
+// inspired by https://www.gamedevelopment.blog/full-libgdx-game-tutorial-entities-ashley/
+class TextureComponent(var region: TextureRegion, var scale: Vector2 = Vector2(1.0f, 1.0f),
+                       var rotation: Float = 0f, var isHidden: Boolean = false) : Component

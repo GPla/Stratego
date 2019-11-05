6bcb68e00f6ec851a5735b1255fc68e707c91758
GPla <36087062+GPla@users.noreply.github.com>
2019-11-05 03:36:26 +0100

    Not needed
    No animation / walking direction

--- core/src/com/mo/stratego/model/component/DirectionComponent.kt
+++
@@ -1,7 +0,0 @@
-package com.mo.stratego.model.component
-
-import com.badlogic.ashley.core.Component
-import com.mo.stratego.model.Direction
-
-class DirectionComponent(var direction: Direction) : Component
-

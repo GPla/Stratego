a04d7043039b3049d242a90b9283964e923edd22
GPla <36087062+GPla@users.noreply.github.com>
2019-11-06 17:38:38 +0100

    Create InvisibleComponent.kt

---
+++ core/src/com/mo/stratego/model/component/InvisibleComponent.kt
@@ -0,0 +1,8 @@
+package com.mo.stratego.model.component
+
+import com.badlogic.ashley.core.Component
+
+/**
+ * Entities with this component will not be rendered
+ */
+class InvisibleComponent : Component

bda97abac60b590078dcef5c91871db7f967ae20
GPla <g-platz@hotmail.de>
2019-11-07 22:50:48 +0100

    changed type of position

--- core/src/com/mo/stratego/model/component/PositionComponent.kt
+++ core/src/com/mo/stratego/model/component/PositionComponent.kt
@@ -1,10 +1,10 @@
 package com.mo.stratego.model.component
 
 import com.badlogic.ashley.core.Component
-import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.GridPoint2
 
 /**
  * A component that specifies the map position at which an entity
  * is rendered. The z component is used to determine the render order.
  */
-class PositionComponent(var position: Vector2, var z: Int = 0) : Component
+class PositionComponent(var position: GridPoint2, var z: Int = 0) : Component

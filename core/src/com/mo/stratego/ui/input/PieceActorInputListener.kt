93044c76a71518704e5a5bcdf2ccc4a3411444c5
GPla <g-platz@hotmail.de>
2019-11-08 16:22:40 +0100

    formatting

--- core/src/com/mo/stratego/ui/input/PieceActorInputListener.kt
+++ core/src/com/mo/stratego/ui/input/PieceActorInputListener.kt
@@ -15,30 +15,36 @@ import com.mo.stratego.ui.PieceActor
 /**
  * Input listener that handles the user input for the [PieceActor] class.
  */
-class PieceActorInputListener(val actor : PieceActor, val engine: Engine) : InputListener() {
-    companion object{
-        val entity : Entity = Entity()
+class PieceActorInputListener(val actor: PieceActor, val engine: Engine) :
+    InputListener() {
+    companion object {
+        val entity: Entity = Entity()
+
         init {
             entity.add(PositionComponent(GridPoint2(10, 10), -1))
-                        .add(TextureComponent(TextureRegion(Texture("pics/highlight_square.png"),64, 64)))
+                    .add(TextureComponent(
+                            TextureRegion(Texture("pics/highlight_square.png"),
+                                          64, 64)))
         }
-        var added : Boolean = false
+
+        var added: Boolean = false
     }
 
     init {
-        if(!added){
+        if (!added) {
             added = true
             engine.addEntity(entity)
         }
     }
 
-    override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int): Boolean {
+    override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int,
+                           button: Int): Boolean {
         Gdx.app.log("dtag", "touched")
         val pos = entity.getComponent(PositionComponent::class.java)
 
         pos.position.x = actor.x.toInt()
         pos.position.y = actor.y.toInt()
 
-        return  true
+        return true
     }
 }

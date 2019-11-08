4f08121cf5d3f77ebb5cef4bbda3136e70e1002e
GPla <g-platz@hotmail.de>
2019-11-08 16:23:09 +0100

    added helper function

--- core/src/com/mo/stratego/model/Range.kt
+++ core/src/com/mo/stratego/model/Range.kt
@@ -1,10 +1,26 @@
 package com.mo.stratego.model
 
 /**
- * Enum of movement ranges
+ * Enum of ranges.
+ * @param range number of field horizontally or vertically
  */
-enum class Range(val range : Int) {
+enum class Range(val range: Int) {
     IMMOBILE(0),    // cannot move
-    NORMAL(1),      // can move 1 field
-    STRAIGHT(9)     // can move any number of fields in a straight line
+    NORMAL(1),      // can move 1 tile
+    ANY(9);         // can move any
+
+
+    companion object {
+        /**
+         * @param rank Rank
+         * @return The [Range] for the [Rank].
+         */
+        fun getRange(rank: Rank): Range {
+            return when (rank) {
+                Rank.FLAG, Rank.BOMB -> IMMOBILE
+                Rank.SCOUT           -> ANY
+                else                 -> NORMAL
+            }
+        }
+    }
 }

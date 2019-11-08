71aadf2e0638260c57e16b691b787367146c2b96
GPla <g-platz@hotmail.de>
2019-11-08 23:01:09 +0100

    removed ownercomponent

--- core/src/com/mo/stratego/model/Piece.kt
+++ core/src/com/mo/stratego/model/Piece.kt
@@ -1,20 +1,16 @@
 package com.mo.stratego.model
 
 import com.badlogic.ashley.core.Entity
-import com.mo.stratego.model.component.OwnerComponent
 import com.mo.stratego.model.component.PieceComponent
 import com.mo.stratego.model.player.Player
 
 /**
  * Class that represents a playing piece. It has a rank and an owner.
  */
-class Piece(val rank: Rank, val owner : Player) : Entity() {
-    val range: Range
-    
+class Piece(val rank: Rank, val owner: Player) : Entity() {
+    val range: Range = Range.getRange(rank)
+
     init {
         add(PieceComponent(this))
-        add(OwnerComponent(owner))
-
-        range = Range.getRange(rank)
     }
 }
--- core/src/com/mo/stratego/model/component/OwnerComponent.kt
+++
@@ -1,6 +0,0 @@
-package com.mo.stratego.model.component
-
-import com.badlogic.ashley.core.Component
-import com.mo.stratego.model.player.Player
-
-class OwnerComponent(var player: Player) : Component

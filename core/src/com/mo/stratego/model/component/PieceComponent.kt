7257d47f2cf6990a9d8d4afcc5a1e13a413dbaf5
GPla <g-platz@hotmail.de>
2019-11-08 16:24:29 +0100

    changed comment

--- core/src/com/mo/stratego/model/component/PieceComponent.kt
+++ core/src/com/mo/stratego/model/component/PieceComponent.kt
@@ -3,4 +3,7 @@ package com.mo.stratego.model.component
 import com.badlogic.ashley.core.Component
 import com.mo.stratego.model.Piece
 
+/**
+ * Wrapper component for the [Piece] class.
+ */
 class PieceComponent(val piece: Piece) : Component

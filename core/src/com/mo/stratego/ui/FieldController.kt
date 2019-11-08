f734364d3a5fd05959daede727d355b11082fd55
GPla <g-platz@hotmail.de>
2019-11-08 19:39:52 +0100

    Update FieldController.kt

--- core/src/com/mo/stratego/ui/FieldController.kt
+++ core/src/com/mo/stratego/ui/FieldController.kt
@@ -3,9 +3,9 @@ package com.mo.stratego.ui
 import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.EntityListener
+import com.badlogic.ashley.core.PooledEngine
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.mo.stratego.GameScreen
 import com.mo.stratego.model.Piece
 
 /**
@@ -15,14 +15,14 @@ import com.mo.stratego.model.Piece
  */
 object FieldController : EntityListener {
 
-    private lateinit var stage : Stage
-    private lateinit var engine : Engine
+    private lateinit var stage: Stage
+    private lateinit var engine: PooledEngine
 
     /**
      * Init the object with this method. If not called before usage
      * an error will be thrown.
      */
-    fun init(stage : Stage, engine : Engine) : FieldController {
+    fun init(stage: Stage, engine: PooledEngine): FieldController {
         this.stage = stage
         this.engine = engine
         return this
@@ -30,7 +30,7 @@ object FieldController : EntityListener {
 
     override fun entityAdded(entity: Entity?) {
         // add if class = piece
-        if(entity is Piece){
+        if (entity is Piece) {
             stage.addActor(PieceActor(entity, engine))
         }
     }

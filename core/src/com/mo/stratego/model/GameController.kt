2eff80e577a9f94c574fcfda29a2d1d080c946ea
GPla <g-platz@hotmail.de>
2019-11-08 16:24:20 +0100

    formatting

--- core/src/com/mo/stratego/model/GameController.kt
+++ core/src/com/mo/stratego/model/GameController.kt
@@ -1,29 +1,26 @@
 package com.mo.stratego.model
 
 import com.badlogic.ashley.core.Engine
-import com.mo.stratego.model.map.Grid
 import com.mo.stratego.model.player.Player
 
 /**
  * TODO: add description
  */
 object GameController {
+    
+    private lateinit var engine: Engine
 
-    private lateinit var engine : Engine
-
-    lateinit var players : Array<Player> // array of players
+    lateinit var players: Array<Player> // array of players
 
     /**
      * Init the object with this method. If not called before usage
      * an exception is thrown.
      */
-    fun init(engine : Engine, player1: Player, player2 : Player) : GameController{
+    fun init(engine: Engine, player1: Player, player2: Player): GameController {
         this.engine = engine
         players = arrayOf(player1, player2)
         return this
     }
-
-
 
 
 }

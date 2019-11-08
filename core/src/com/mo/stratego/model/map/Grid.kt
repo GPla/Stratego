74c2301197a3da932a4eb251f5b850799e64d551
GPla <g-platz@hotmail.de>
2019-11-08 16:24:00 +0100

    added helper for allowed moves

--- core/src/com/mo/stratego/model/map/Grid.kt
+++ core/src/com/mo/stratego/model/map/Grid.kt
@@ -5,44 +5,57 @@ import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.EntityListener
 import com.badlogic.gdx.math.GridPoint2
 import com.mo.stratego.model.Piece
+import com.mo.stratego.model.Range
 import com.mo.stratego.model.component.MoveComponent
 import com.mo.stratego.model.component.PositionComponent
+import com.mo.stratego.model.player.Player
 
 /**
- * This class represents the game grid.
+ * This class represents the game grid. The [EntityListener] is
+ * triggered if a [Piece] has a [PositionComponent] and
+ * independently, another time, if it has a [MoveComponent].
  */
+//TODO: piece defeated
 object Grid : EntityListener {
 
     // playing grid with origin in the bottom left corner, same as camera
-    private val matrix: Array<Array<Piece?>> = Array(10) { arrayOfNulls<Piece?>(10) }
+    private val matrix: Array<Array<Piece?>> =
+            Array(10) { arrayOfNulls<Piece?>(10) }
 
-    private val posMapper: ComponentMapper<PositionComponent> = ComponentMapper.getFor(PositionComponent::class.java)
-    private val moveMapper: ComponentMapper<MoveComponent> = ComponentMapper.getFor(MoveComponent::class.java)
-
-    override fun entityRemoved(entity: Entity?) {
-        //TODO: remove from grid
-        //TODO: check for inconsistencies of movecomponent
-    }
+    // component mapper for faster access
+    private val posMapper: ComponentMapper<PositionComponent> =
+            ComponentMapper.getFor(PositionComponent::class.java)
+    private val moveMapper: ComponentMapper<MoveComponent> =
+            ComponentMapper.getFor(MoveComponent::class.java)
 
     /**
-     * Update grid if a [Piece] with [PositionComponent] or
-     * [MoveComponent] is added.
+     * Updates the grid if a [MoveComponent] from a [Piece] is removed.
+     * @param entity Entity
      */
-    override fun entityAdded(entity: Entity?) {
-        // check if playing piece
+    override fun entityRemoved(entity: Entity?) {
+        // safety check
         if (entity !is Piece)
             return
 
-        // check for movement
+        update(entity)
+    }
+
+    /**
+     * Updates the grid if a [Piece] with [PositionComponent] and
+     * no [MoveComponent] was added.
+     * @param entity Entity
+     */
+    override fun entityAdded(entity: Entity?) {
+        // safety check
+        if (entity !is Piece)
+            return
+
+        // ignore movement for now
         val move = moveMapper.get(entity)
         if (move != null)
-            update(move, entity)
-        else {
-            //check position
-            val pos = posMapper.get(entity)
-            if (pos != null)
-                update(pos, entity)
-        }
+            return
+
+        update(entity)
     }
 
     /**
@@ -53,47 +66,49 @@ object Grid : EntityListener {
     }
 
     /**
-     * Updates grid with [MoveComponent]
-     * @param move MoveComponent
+     * Updates the grid.
      * @param piece Piece
      */
-    private fun update(move: MoveComponent, piece: Piece) {
-        // get piece position
-        val position = posMapper.get(piece) ?: return
-        val point = translatePositionToCell(position.position)
-        matrix[point.x][point.y] = piece
+    private fun update(piece: Piece) {
+        val position = posMapper.get(piece)?.position
+        if (position != null) { // entered family
+            // add piece to board
+            val point = translatePositionToCell(position)
+            matrix[point.x][point.y] = piece
+        } else { // removed from family
+            // remove piece from board
+            for (y in 0..9) {
+                for (x in 0..9) {
+                    if (matrix[x][y] == piece) {
+                        matrix[x][y] = null
+                        break
+                    }
+                }
+            }
+        }
     }
 
     /**
-     * Updates grid with [PositionComponent]
-     * @param position PositionComponent
-     * @param piece Piece
-     */
-    private fun update(position: PositionComponent, piece: Piece) {
-        val point = translatePositionToCell(position.position)
-        matrix[point.x][point.y] = piece
-    }
-
-    /**
-     * Calculates the grid position with the origin in
-     * the bottom left corner, same as camera.
+     * Calculates the grid position; the origin is in the bottom left corner,
+     * same as camera.
      * @param position Position
-     * @return the grid position of the [Piece]
+     * @return the grid position of the piece.
      */
     private fun translatePositionToCell(position: GridPoint2): GridPoint2 {
         return GridPoint2(position.x - GameMap.gridLeft,
-                matrix.size - (position.y - GameMap.gridBottom) - 1)
+                          matrix.size - (position.y - GameMap.gridBottom) - 1)
     }
 
 
     /**
-     * @return String of occupied cells.
+     * @return  A string representation of the grid. A zero indicates an
+     * empty cell otherwise the [Player]'s id.
      */
     override fun toString(): String {
         val builder: StringBuilder = StringBuilder()
         for (y in 0..9) {
             for (x in 0..9) {
-                builder.append("${if (matrix[x][y] != null) 1 else 0} ")
+                builder.append("${matrix[x][y]?.owner?.id ?: 0} ")
             }
             builder.appendln()
         }
@@ -101,9 +116,10 @@ object Grid : EntityListener {
     }
 
     /**
-     *  Overload of the bracket operator. Expects the tilemap coordinates.
-     *  @param position GridPoint2
-     * @return [Piece] at the grid cell. Returns null if cell is empty or
+     * Overload of the bracket operator. Expects the tilemap coordinates.
+     * Call: Grid[Gridpoint2(x, y)]
+     * @param position GridPoint2
+     * @return [Piece] in the grid cell. Returns null if cell is empty or
      * indices are out of bound.
      */
     operator fun get(position: GridPoint2): Piece? {
@@ -111,9 +127,62 @@ object Grid : EntityListener {
 
         // out of bound
         if (point.x < 0 || point.x >= matrix.size ||
-                point.y < 0 || point.y >= matrix.size)
+            point.y < 0 || point.y >= matrix.size)
             return null
 
         return matrix[point.x][point.y]
     }
+
+
+    /**
+     * @param piece Piece
+     * @return A list of allowed moves for the piece.
+     */
+    fun getAllowedMoves(piece: Piece): List<GridPoint2> {
+        // check if piece is on board
+        val position = posMapper.get(piece)?.position ?: return emptyList()
+
+        val standpoint = translatePositionToCell(position)
+        val moves = getPossibleMoves(piece.range)
+
+        val allowedMoves = mutableListOf<GridPoint2>()
+
+        for (move in moves) {
+            if (isCellAllowed(standpoint.add(move), piece.owner))
+                allowedMoves.add(move)
+        }
+        return allowedMoves
+    }
+
+    /**
+     * @param range Range
+     * @return A list of possible moves in horizontal and vertical direction.
+     */
+    private fun getPossibleMoves(range: Range): List<GridPoint2> {
+        val list: MutableList<GridPoint2> = mutableListOf()
+        for (r in 1..range.range) {
+            val directions =
+                    listOf(GridPoint2(r, 0), GridPoint2(-r, 0),
+                           GridPoint2(0, r), GridPoint2(0, -r))
+            list.addAll(directions)
+        }
+        return list
+    }
+
+    /**
+     * @param point Grid position of the [Piece]
+     * @param owner owner of the [Piece]
+     * @return Whether or not the cell can be occupied by a [Piece].
+     */
+    private fun isCellAllowed(point: GridPoint2, owner: Player): Boolean {
+        // lakes
+        if ((point.x in 2..3 || point.x in 6..7) && point.y in 4..5)
+            return false
+
+        // check if cell is blocked by a piece of the same owner
+        val owner2 = Grid[point]?.owner ?: false
+        return owner != owner2
+    }
+
+
 }

4ee417ee38945df912f402f75e18e71fb544eb7f
GPla <36087062+GPla@users.noreply.github.com>
2019-11-09 20:26:37 +0100

    added description

--- core/src/com/mo/stratego/model/component/HighlightComponent.kt
+++ core/src/com/mo/stratego/model/component/HighlightComponent.kt
@@ -1,11 +1,16 @@
 package com.mo.stratego.model.component
 
 import com.badlogic.ashley.core.Component
+import com.badlogic.ashley.core.Entity
 import com.badlogic.gdx.math.GridPoint2
 import com.mo.stratego.model.HighlightType
 import com.mo.stratego.model.Piece
 
-
-//TODO: add description
+/**
+ * Class that marks an [Entity] as highlight. An highlight is a visual
+ * cue that marks a possible move for a [Piece].
+ * @property type Type of highlight
+ * @property piece Piece
+ */
 class HighlightComponent(val type: HighlightType, val piece: Piece,
                          val move: GridPoint2?) : Component

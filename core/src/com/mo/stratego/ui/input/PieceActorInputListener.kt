47ac7f378c576489871a9f2c3b8ed32a538acfe7
GPla <g-platz@hotmail.de>
2019-11-09 13:18:45 +0100

    fixed highlighting

--- core/src/com/mo/stratego/ui/input/PieceActorInputListener.kt
+++ core/src/com/mo/stratego/ui/input/PieceActorInputListener.kt
@@ -2,14 +2,11 @@ package com.mo.stratego.ui.input
 
 import com.badlogic.ashley.core.ComponentMapper
 import com.badlogic.ashley.core.PooledEngine
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.math.GridPoint2
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
+import com.mo.stratego.model.HighlightType
 import com.mo.stratego.model.component.PositionComponent
-import com.mo.stratego.model.component.TextureComponent
 import com.mo.stratego.model.map.Grid
 import com.mo.stratego.ui.PieceActor
 
@@ -24,37 +21,35 @@ class PieceActorInputListener(val actor: PieceActor, val engine: PooledEngine) :
 
     override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int,
                            button: Int): Boolean {
-        Gdx.app.log("dtag", "touched")
 
+        HighlightType.deleteHighlight(engine)
         createHighlight()
 
         return true
     }
 
-    //FIXME: textures
+    /**
+     * Creates the highlight for the possible moves of the entity.
+     */
     private fun createHighlight() {
         val standpoint = posMapper.get(actor.piece)?.position ?: return
         val allowedMoves = Grid.getAllowedMoves(actor.piece)
 
-        //FIXME: texture management
-        val circleTexture = TextureComponent(
-                TextureRegion(Texture("pics/highlight_circle.png")))
-        val squareTexture = TextureComponent(
-                TextureRegion(Texture("pics/highlight_square.png")))
 
         // selected highlight square
-        engine.addEntity(engine.createEntity()
-                                 .add(squareTexture)
-                                 .add(PositionComponent(standpoint, -1)))
+        val square =
+                engine.createEntity().add(PositionComponent(standpoint, -1))
+        engine.addEntity(square)
+        HighlightType.createHightlight(square, HighlightType.SQUARE)
 
-        Gdx.app.log("dtag", "size: ${allowedMoves.size}")
-        // move highlight circle
+        // highlight circle
         for (move in allowedMoves) {
             val point = GridPoint2(standpoint).add(move)
-            engine.addEntity(engine.createEntity()
-                                     .add(circleTexture)
-                                     .add(PositionComponent(point, 1)))
+            val circle = engine.createEntity().add(PositionComponent(point, 1))
+            engine.addEntity(circle)
+            HighlightType.createHightlight(circle, HighlightType.CIRCLE)
         }
     }
+
 
 }

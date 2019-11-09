9befc1b5b3e35bbd76d88d095cba2fcf620665a2
GPla <g-platz@hotmail.de>
2019-11-09 13:17:37 +0100

    Update GameScreen.kt

--- core/src/com/mo/stratego/GameScreen.kt
+++ core/src/com/mo/stratego/GameScreen.kt
@@ -4,12 +4,11 @@ import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Family
 import com.badlogic.ashley.core.PooledEngine
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputMultiplexer
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.math.GridPoint2
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Stage
@@ -20,13 +19,12 @@ import com.mo.stratego.model.Rank
 import com.mo.stratego.model.component.MoveComponent
 import com.mo.stratego.model.component.PieceComponent
 import com.mo.stratego.model.component.PositionComponent
-import com.mo.stratego.model.component.TextureComponent
 import com.mo.stratego.model.map.GameMap
 import com.mo.stratego.model.map.Grid
-import com.mo.stratego.model.player.PlayerLocal
-import com.mo.stratego.model.player.PlayerProxy
+import com.mo.stratego.model.player.PlayerType
 import com.mo.stratego.model.system.RenderSystem
 import com.mo.stratego.ui.FieldController
+import com.mo.stratego.ui.input.MapListener
 
 /**
  * Game screen
@@ -39,9 +37,8 @@ class GameScreen : Screen {
     private var camera: OrthographicCamera = OrthographicCamera()
     private val engine: Engine
     private val batch: SpriteBatch
-    private val stage: Stage
 
-    //TODO: fix comments
+    //TODO: fix comments and cleanup
     init {
         //set camera to map dimensions, to show full map
         camera.setToOrtho(false, GameMap.width.toFloat(),
@@ -52,16 +49,16 @@ class GameScreen : Screen {
 
         batch = SpriteBatch()
 
-        stage = Stage(StretchViewport(camera.viewportWidth,
-                                      camera.viewportHeight))
-
+        val objectStage = Stage(StretchViewport(camera.viewportWidth,
+                                                camera.viewportHeight))
         engine = PooledEngine()
 
         // add listeners to engine
         //listener is triggered if entity component is added / removed from the family
         // FieldController
+        FieldController.init(objectStage, engine)
         var family = Family.all(PieceComponent::class.java).get()
-        engine.addEntityListener(family, FieldController.init(stage, engine))
+        engine.addEntityListener(family, FieldController)
 
         // Grid
         family = Family.all(PieceComponent::class.java,
@@ -73,14 +70,14 @@ class GameScreen : Screen {
         engine.addEntityListener(family, Grid)
 
         GameMap.engine = engine
-        GameController.init(engine, PlayerLocal(1), PlayerProxy(2))
+        GameController.init(engine, PlayerType.LOCAL, PlayerType.PROXY)
 
         // add systems to engine
         engine.addSystem(RenderSystem(batch, camera))
 
 
         val piece = Piece(Rank.MARSHAL, GameController.players[0])
-                        .add(PositionComponent(GridPoint2(3, 7)))
+                .add(PositionComponent(GridPoint2(3, 7)))
 
         engine.addEntity(piece)
 
@@ -94,10 +91,10 @@ class GameScreen : Screen {
 
 
         // handle user input with stage
-        Gdx.input.inputProcessor = stage
-
-        Gdx.app.log("dtag", Grid.toString())
-
+        // objectStage receive events, if not handled they get passed to
+        // the map stage
+        Gdx.input.inputProcessor =
+                InputMultiplexer(objectStage, MapListener(engine))
     }
 
 
@@ -109,6 +106,7 @@ class GameScreen : Screen {
     }
 
     override fun render(delta: Float) {
+        Gdx.app.log("dtag", Grid.toString())
 
         // clear screen
         Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
@@ -122,10 +120,12 @@ class GameScreen : Screen {
         // update ashley engine
         engine.update(Gdx.graphics.deltaTime)
 
-        // render stage
-        stage.batch.projectionMatrix = camera.combined
-        stage.act(delta)
-        stage.draw()
+        // render stages
+        FieldController.stage.run {
+            batch.projectionMatrix = camera.combined
+            act(delta)
+            draw()
+        }
     }
 
     override fun pause() {

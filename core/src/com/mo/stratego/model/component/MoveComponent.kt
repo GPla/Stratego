3a0b1c5d280a9af4f087572405049db4fdcf60ab
GPla <36087062+GPla@users.noreply.github.com>
2019-11-09 22:01:43 +0100

    added description

--- core/src/com/mo/stratego/model/component/MoveComponent.kt
+++ core/src/com/mo/stratego/model/component/MoveComponent.kt
@@ -2,6 +2,10 @@ package com.mo.stratego.model.component
 
 import com.badlogic.ashley.core.Component
 import com.badlogic.gdx.math.GridPoint2
+import com.mo.stratego.model.Piece
 
-
+/**
+ * This component defines a move action for a [Piece]. The
+ * move property will be added to the [Piece]'s position.
+ */
 class MoveComponent(val move: GridPoint2) : Component

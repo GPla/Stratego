2e6a5888017b7912739908ac3060c704192d8b7e
GPla <g-platz@hotmail.de>
2019-11-09 16:23:47 +0100

    renamed

--- core/src/com/mo/stratego/ui/input/PieceInputListener.kt
+++ core/src/com/mo/stratego/ui/input/PieceInputListener.kt
@@ -1,23 +1,25 @@
 package com.mo.stratego.ui.input
 
 import com.badlogic.ashley.core.ComponentMapper
-import com.badlogic.ashley.core.PooledEngine
+import com.badlogic.ashley.core.Engine
 import com.badlogic.gdx.math.GridPoint2
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.mo.stratego.model.HighlightType
+import com.mo.stratego.model.Piece
+import com.mo.stratego.model.component.PieceComponent
 import com.mo.stratego.model.component.PositionComponent
 import com.mo.stratego.model.map.Grid
-import com.mo.stratego.ui.PieceActor
 
 /**
- * Input listener that handles the user input for the [PieceActor] class.
+ * Input listener that handles the user input for the [Piece] class.
  */
-class PieceActorInputListener(val actor: PieceActor, val engine: PooledEngine) :
-    InputListener() {
+class PieceInputListener(private val piece: Piece,
+                         private val engine: Engine) : InputListener() {
 
     private val posMapper =
             ComponentMapper.getFor(PositionComponent::class.java)
+    private val pieceMapper = ComponentMapper.getFor(PieceComponent::class.java)
 
     override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int,
                            button: Int): Boolean {
@@ -32,22 +34,24 @@ class PieceActorInputListener(val actor: PieceActor, val engine: PooledEngine) :
      * Creates the highlight for the possible moves of the entity.
      */
     private fun createHighlight() {
-        val standpoint = posMapper.get(actor.piece)?.position ?: return
-        val allowedMoves = Grid.getAllowedMoves(actor.piece)
+        val standpoint = posMapper.get(piece)?.position ?: return
+        val allowedMoves = Grid.getAllowedMoves(piece)
 
 
         // selected highlight square
         val square =
                 engine.createEntity().add(PositionComponent(standpoint, -1))
         engine.addEntity(square)
-        HighlightType.createHightlight(square, HighlightType.SQUARE)
+        HighlightType.createHightlight(square, piece, HighlightType.SQUARE,
+                                       null)
 
         // highlight circle
         for (move in allowedMoves) {
             val point = GridPoint2(standpoint).add(move)
             val circle = engine.createEntity().add(PositionComponent(point, 1))
             engine.addEntity(circle)
-            HighlightType.createHightlight(circle, HighlightType.CIRCLE)
+            HighlightType.createHightlight(circle, piece, HighlightType.CIRCLE,
+                                           move)
         }
     }
 

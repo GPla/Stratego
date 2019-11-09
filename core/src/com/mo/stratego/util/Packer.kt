15666a47bb58dc4aaaaca5597f87f34fb4b5c1d9
GPla <36087062+GPla@users.noreply.github.com>
2019-11-09 09:25:02 +0100

    added texturepacker

---
+++ core/src/com/mo/stratego/util/Packer.kt
@@ -0,0 +1,10 @@
+package com.mo.stratego.util
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker
+
+
+fun main(){
+    TexturePacker.process("C:\\Users\\g-pla\\AndroidStudioProjects\\Stratego\\android\\assets\\pics",
+            "C:\\Users\\g-pla\\AndroidStudioProjects\\Stratego\\android\\assets",
+            "pics")
+}

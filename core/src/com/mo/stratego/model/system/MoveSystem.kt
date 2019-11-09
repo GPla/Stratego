bbb122aca0a0e913f653a6be9ef03d139de0b37c
GPla <36087062+GPla@users.noreply.github.com>
2019-11-09 22:02:54 +0100

    fixed movement

--- core/src/com/mo/stratego/model/system/MoveSystem.kt
+++ core/src/com/mo/stratego/model/system/MoveSystem.kt
@@ -4,32 +4,49 @@ import com.badlogic.ashley.core.ComponentMapper
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.Family
 import com.badlogic.ashley.systems.IteratingSystem
+import com.mo.stratego.model.component.AttackComponent
 import com.mo.stratego.model.component.MoveComponent
+import com.mo.stratego.model.component.PieceComponent
 import com.mo.stratego.model.component.PositionComponent
+import com.mo.stratego.model.map.Grid
 
-class MoveSystem : IteratingSystem(Family.all(PositionComponent::class.java,
-                                              MoveComponent::class.java).get()) {
+class MoveSystem : IteratingSystem(
+        Family.all(PieceComponent::class.java, PositionComponent::class.java,
+                   MoveComponent::class.java).get()) {
 
     // component mapper
     private val posMapper =
             ComponentMapper.getFor(PositionComponent::class.java)
     private val movMapper = ComponentMapper.getFor(MoveComponent::class.java)
+    private val pieceMapper = ComponentMapper.getFor(PieceComponent::class.java)
 
     override fun processEntity(entity: Entity?, deltaTime: Float) {
-        val position = posMapper.get(entity)
-        val move = movMapper.get(entity)
+        val position = posMapper.get(entity)?.position
+        val move = movMapper.get(entity)?.move
+        val piece = pieceMapper.get(entity)?.piece
 
         if (entity == null ||
             position == null ||
-            move == null)
+            move == null ||
+            piece == null)
             return
 
-        // check if move is valid 
-        if ()
-        // move entity to target position
-            position.position.add(move.move)
+        val newPoint = position.cpy().add(move)
 
-        entity.remove(MoveComponent::class.java)
-
+        //FIXME: beautify
+        // check if move is valid
+        when (Grid.isCellAllowed(Grid.translatePositionToCell(newPoint),
+                                 piece.owner)) {
+            0 -> piece.remove(MoveComponent::class.java)
+            1 -> {
+                position.add(move)
+                piece.remove(MoveComponent::class.java)
+            }
+            2 -> {
+                val enemy = Grid[newPoint]
+                if (enemy != null)
+                    piece.add(AttackComponent(enemy))
+            }
+        }
     }
 }

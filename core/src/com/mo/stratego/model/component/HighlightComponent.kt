580d473a54cf036e65fe2531d13ab838f1688397
GPla <g-platz@hotmail.de>
2019-11-09 13:17:33 +0100

    added type

--- core/src/com/mo/stratego/model/component/HighlightComponent.kt
+++ core/src/com/mo/stratego/model/component/HighlightComponent.kt
@@ -1,9 +1,10 @@
 package com.mo.stratego.model.component
 
 import com.badlogic.ashley.core.Component
+import com.mo.stratego.model.HighlightType
 
 
 /**
  * This component serves no other purpose than as flag for filtering.
  */
-class HighlightComponent : Component
+class HighlightComponent(val type: HighlightType) : Component

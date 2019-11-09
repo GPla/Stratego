72d493a2d12bc9d2e4aacc97a427863989420b6a
GPla <g-platz@hotmail.de>
2019-11-09 16:23:34 +0100

    Create MoveSystem.kt

---
+++ core/src/com/mo/stratego/model/system/MoveSystem.kt
@@ -0,0 +1,33 @@
+package com.mo.stratego.model.system
+
+import com.badlogic.ashley.core.ComponentMapper
+import com.badlogic.ashley.core.Entity
+import com.badlogic.ashley.core.Family
+import com.badlogic.ashley.systems.IteratingSystem
+import com.mo.stratego.model.component.MoveComponent
+import com.mo.stratego.model.component.PositionComponent
+
+class MoveSystem : IteratingSystem(Family.all(PositionComponent::class.java,
+                                              MoveComponent::class.java).get()) {
+
+    // component mapper
+    private val posMapper =
+            ComponentMapper.getFor(PositionComponent::class.java)
+    private val movMapper = ComponentMapper.getFor(MoveComponent::class.java)
+
+    override fun processEntity(entity: Entity?, deltaTime: Float) {
+        val position = posMapper.get(entity)
+        val move = movMapper.get(entity)
+
+        if (entity == null ||
+            position == null ||
+            move == null)
+            return
+
+        // move entity to target position
+        position.position.add(move.move)
+
+        entity.remove(MoveComponent::class.java)
+
+    }
+}

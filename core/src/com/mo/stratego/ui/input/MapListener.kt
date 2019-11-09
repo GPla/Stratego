379a10fed7f9d78f19b49c055e8da149f36071a1
GPla <g-platz@hotmail.de>
2019-11-09 13:54:40 +0100

    added comment

--- core/src/com/mo/stratego/ui/input/MapListener.kt
+++ core/src/com/mo/stratego/ui/input/MapListener.kt
@@ -3,7 +3,14 @@ package com.mo.stratego.ui.input
 import com.badlogic.ashley.core.Engine
 import com.badlogic.gdx.InputAdapter
 import com.mo.stratego.model.HighlightType
+import com.mo.stratego.ui.FieldController
 
+/**
+ * Listener that is activated if a click is not handled by
+ * the [FieldController]'s stage.
+ * @property engine Engine
+ * @constructor
+ */
 class MapListener(val engine: Engine) : InputAdapter() {
 
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int,

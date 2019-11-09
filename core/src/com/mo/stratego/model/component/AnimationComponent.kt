18df8350fc3c42e6dd46ecda81c80f8aaf60867e
GPla <36087062+GPla@users.noreply.github.com>
2019-11-09 22:01:09 +0100

    Delete AnimationComponent.kt

--- core/src/com/mo/stratego/model/component/AnimationComponent.kt
+++
@@ -1,8 +0,0 @@
-package com.mo.stratego.model.component
-
-import com.badlogic.ashley.core.Component
-
-// inspired by https://www.gamedevelopment.blog/full-libgdx-game-tutorial-entities-ashley/
-class AnimationComponent : Component {
-}
-// direction

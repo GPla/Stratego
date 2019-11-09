bf352acd0e3fa24b8522392e4a5e874da0e83f42
GPla <g-platz@hotmail.de>
2019-11-09 16:23:27 +0100

    changed to be a generic actor

--- core/src/com/mo/stratego/ui/FieldActor.kt
+++ core/src/com/mo/stratego/ui/FieldActor.kt
@@ -1,36 +1,30 @@
 package com.mo.stratego.ui
 
-import com.badlogic.ashley.core.PooledEngine
+import com.badlogic.ashley.core.Entity
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.scenes.scene2d.Actor
-import com.mo.stratego.model.Piece
+import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.mo.stratego.model.component.PositionComponent
 import com.mo.stratego.model.component.TextureComponent
 import com.mo.stratego.model.system.RenderSystem
-import com.mo.stratego.ui.input.PieceActorInputListener
 import com.mo.stratego.util.Constants
 
 /**
- * Scene2d actor for the [Piece] class. This class links an [Piece] instance
+ * Scene2d actor for the [Entity] class. This class links an [Entity] instance
  * with an [Actor] instance to handle the user input. It does not draw on the
  * screen.
  */
-class PieceActor(val piece: Piece, val engine: PooledEngine) : Actor() {
-
-    init {
-        // add listener to handle user input
-        addListener(PieceActorInputListener(this, engine))
-    }
+class FieldActor(val entity: Entity) : Actor() {
 
     /**
-     * This method updates the bounds to the position and dimensions of the [Piece],
-     * so that the[PieceActorInputListener] receives the relevant user input, i.e touch events.
-     * It does not draw the texture of the [Piece], this is done by the [RenderSystem].
+     * This method updates the bounds to the position and dimensions of the [Entity],
+     * so that the [InputListener] receives the relevant user input, i.e touch events.
+     * It does not draw the texture of the [Entity], this is done by the [RenderSystem].
      */
     override fun draw(batch: Batch?, parentAlpha: Float) {
         // get necessary components for drawing
-        val position = piece.getComponent(PositionComponent::class.java)
-        val texture = piece.getComponent(TextureComponent::class.java)
+        val position = entity.getComponent(PositionComponent::class.java)
+        val texture = entity.getComponent(TextureComponent::class.java)
 
         //check if components are valid
         if (position == null || texture == null)

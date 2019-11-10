827f65db352819be4825ea388c39dfe9ee4fefae
GPla <g-platz@hotmail.de>
2019-11-10 23:00:38 +0100

    added texture methods

--- core/src/com/mo/stratego/model/Piece.kt
+++ core/src/com/mo/stratego/model/Piece.kt
@@ -9,14 +9,52 @@ import com.mo.stratego.model.player.Player
  */
 class Piece(val rank: Rank, val owner: Player) : Entity() {
     val range: Range = Range.getRange(rank)
+    private var frontSide: Boolean = owner.id == 0 // true if front is shown
+    private val defaultSide: Side = if (owner.id == 0) Side.FRONT else Side.BACK
 
     init {
         add(PieceComponent(this))
 
-        //TODO: change?
+        // add texture
         when (owner.id) {
             0 -> add(Atlas.getPieceTexture(rank, 0))
-            1 -> add(Atlas.getPieceTexture(rank, 1))
+            1 -> add(Atlas.getPieceBacksideTexture(1))
         }
+    }
+
+    /**
+     * Shows front side texture
+     */
+    fun showFront() {
+        if (!frontSide)
+            add(Atlas.getPieceTexture(rank, owner.id))
+        frontSide = true
+    }
+
+    /**
+     * Shows back side texture
+     */
+    fun showBack() {
+        if (frontSide)
+            add(Atlas.getPieceBacksideTexture(owner.id))
+        frontSide = false
+    }
+
+    /**
+     * Shows default side texture
+     */
+    fun showDefault() {
+        when (defaultSide) {
+            Side.FRONT -> showFront()
+            Side.BACK  -> showBack()
+        }
+    }
+
+    /**
+     * @return Whether or not the piece should receive touch events
+     */
+    //TODO: disable touch when moving and piece has move selected
+    fun isTouchable(): Boolean {
+        return owner.allowTouch
     }
 }

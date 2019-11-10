1c77b3bf6c1e15c8098f44ec38fc988ccbb929de
GPla <g-platz@hotmail.de>
2019-11-10 23:00:49 +0100

    added allowTouch

--- core/src/com/mo/stratego/model/player/Player.kt
+++ core/src/com/mo/stratego/model/player/Player.kt
@@ -1,4 +1,15 @@
 package com.mo.stratego.model.player
 
+//TODO desc
+/**
+ * Inspired by https://pdfs.semanticscholar.org/f35c/df2b5cb1a36d703ab6c4a4d80cbaaf3cc603.pdf
+ * @property id Int
+ * @property allowTouch Boolean
+ * @constructor
+ */
 abstract class Player(val id: Int) {
+
+    abstract val allowTouch: Boolean
+
+    //TODO: implement https://pdfs.semanticscholar.org/f35c/df2b5cb1a36d703ab6c4a4d80cbaaf3cc603.pdf
 }

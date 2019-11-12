bfeabab7943fb7be2f9b165863135d2ab41878ad
GPla <g-platz@hotmail.de>
2019-11-12 22:19:31 +0100

    added ++ operator

--- core/src/com/mo/stratego/model/GameState.kt
+++ core/src/com/mo/stratego/model/GameState.kt
@@ -9,4 +9,19 @@ enum class GameState {
     TURN_PLAYER_1,      // Turn player 1
     TURN_PLAYER_2,      // Turn player 2
     GAME_OVER;          // Game ended
+
+    /**
+     * Overrides the ++ operator.
+     * A call of this operator sets the next game state.
+     * @return Next GameState
+     */
+    operator fun inc(): GameState {
+        return when (this) {
+            INIT          -> PREPARATION
+            PREPARATION   -> TURN_PLAYER_1
+            TURN_PLAYER_1 -> TURN_PLAYER_2
+            TURN_PLAYER_2 -> TURN_PLAYER_1
+            GAME_OVER     -> GAME_OVER
+        }
+    }
 }

c771424ad41b1579dfc8bbe01f1ff1e9303a5286
GPla <36087062+GPla@users.noreply.github.com>
2019-11-12 09:09:18 +0100

    implemented player interface

--- core/src/com/mo/stratego/model/player/Player.kt
+++ core/src/com/mo/stratego/model/player/Player.kt
@@ -1,5 +1,7 @@
 package com.mo.stratego.model.player
 
+import com.mo.stratego.model.Move
+
 //TODO desc
 /**
  * Inspired by https://pdfs.semanticscholar.org/f35c/df2b5cb1a36d703ab6c4a4d80cbaaf3cc603.pdf
@@ -9,7 +11,26 @@ package com.mo.stratego.model.player
  */
 abstract class Player(val id: Int) {
 
-    abstract val allowTouch: Boolean
+    /**
+     * Whether or not the player is allowed to make his move.
+     */
+    var allow: Boolean = false
 
-    //TODO: implement https://pdfs.semanticscholar.org/f35c/df2b5cb1a36d703ab6c4a4d80cbaaf3cc603.pdf
+    /**
+     * The move made by the player.
+     */
+    var move: Move? = null
+
+    /**
+     * The move made by the other player.
+     */
+    var othersMove: Move? = null
+
+    /**
+     * Presents the other players move.
+     */
+    abstract fun present()
+
+    //TODO: outcome??
+
 }

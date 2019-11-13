0ddefbc482e50d2b8ea54d89455dc4ccb97bee0a
GPla <36087062+GPla@users.noreply.github.com>
2019-11-13 22:33:15 +0100

    Create HudController.kt

---
+++ core/src/com/mo/stratego/ui/HudController.kt
@@ -0,0 +1,13 @@
+package com.mo.stratego.ui
+
+import com.badlogic.ashley.core.PooledEngine
+import com.badlogic.gdx.scenes.scene2d.Stage
+
+//TODO: implement piece placer
+object HudController {
+
+    fun init(stage: Stage, engine: PooledEngine) {
+
+    }
+
+}

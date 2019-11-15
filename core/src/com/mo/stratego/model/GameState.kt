1aa5e5c1eda254b38bb4af5cde0df801a9332b43
GPla <36087062+GPla@users.noreply.github.com>
2019-11-15 21:12:40 +0100

    added state

--- core/src/com/mo/stratego/model/GameState.kt
+++ core/src/com/mo/stratego/model/GameState.kt
@@ -4,11 +4,12 @@ package com.mo.stratego.model
  * Enum of game states
  */
 enum class GameState {
-    INIT,               // Initialization
-    PREPARATION,        // Preparation
-    TURN_PLAYER_1,      // Turn player 1
-    TURN_PLAYER_2,      // Turn player 2
-    GAME_OVER;          // Game ended
+    INIT,                   // Initialization
+    PREPARATION_PLAYER_1,   // Preparation player 1
+    PREPARATION_PLAYER_2,   // Preparation player 2
+    TURN_PLAYER_1,          // Turn player 1
+    TURN_PLAYER_2,          // Turn player 2
+    GAME_OVER;              // Game ended
 
     /**
      * Overrides the ++ operator.
@@ -17,11 +18,12 @@ enum class GameState {
      */
     operator fun inc(): GameState {
         return when (this) {
-            INIT          -> PREPARATION
-            PREPARATION   -> TURN_PLAYER_1
-            TURN_PLAYER_1 -> TURN_PLAYER_2
-            TURN_PLAYER_2 -> TURN_PLAYER_1
-            GAME_OVER     -> GAME_OVER
+            INIT                 -> PREPARATION_PLAYER_1
+            PREPARATION_PLAYER_1 -> PREPARATION_PLAYER_2
+            PREPARATION_PLAYER_2 -> TURN_PLAYER_1
+            TURN_PLAYER_1        -> TURN_PLAYER_2
+            TURN_PLAYER_2        -> TURN_PLAYER_1
+            GAME_OVER            -> GAME_OVER
         }
     }
 }

4bd40b242308b76689317b768e554114ab009929
GPla <g-platz@hotmail.de>
2019-11-19 20:05:57 +0100

    added state event

--- core/src/com/mo/stratego/ui/HudController.kt
+++ core/src/com/mo/stratego/ui/HudController.kt
@@ -2,10 +2,13 @@ package com.mo.stratego.ui
 
 import com.badlogic.ashley.core.PooledEngine
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.mo.stratego.util.StateEvent
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 
 //TODO: implement piece placer
@@ -13,6 +16,8 @@ object HudController {
 
     lateinit var stage: Stage
     private lateinit var engine: PooledEngine
+
+    private val actorMap: MutableMap<String, Actor> = mutableMapOf()
 
     /**
      * Init the object with this method. If not called before usage
@@ -29,25 +34,29 @@ object HudController {
     }
 
 
-    fun initStage() {
-        val atlas =
-                TextureAtlas(Gdx.files.internal("ui/default/skin/uiskin.atlas"))
-        val skin =
-                Skin(Gdx.files.internal("ui/default/skin/uiskin.json"), atlas)
+    private fun initStage() {
+        val skin = Skin(Gdx.files.internal("ui/default/skin/uiskin.json"))
+        val lab = Label("Text", skin)
 
-        //val pieces = ImageList(skin)
-        //pieces.setPosition(0f, 0f)
-        //pieces.height = 10f
-        //pieces.width = 10f
-        //engine.entities.filter { it is Piece }
-        //.forEach { pieces.items.add(it as Piece) }
-        //stage.addActor(pieces)
+        lab.setPosition((Gdx.graphics.width / 2f), Gdx.graphics.height / 2f)
+        lab.setFontScale(2f)
 
-        val button = TextButton("Click", skin, "default")
-        button.height = 1f
-        button.width = 2f
+        stage.addActor(lab)
+        actorMap.put("label", lab)
 
-        stage.addActor(button)
+        // TODO add button
+        // TODO add animation and make text fancier
+    }
+
+    @Subscribe(threadMode = ThreadMode.ASYNC)
+    fun onStateEvent(msg: StateEvent) {
+        Gdx.app.log("state", "state: ${msg.state}")
+        val lab = actorMap.getValue("label") as Label
+        lab.setText(msg.state)
+        lab.setPosition((Gdx.graphics.width - lab.width) / 2f,
+                        Gdx.graphics.height / 2f)
 
     }
+
+
 }

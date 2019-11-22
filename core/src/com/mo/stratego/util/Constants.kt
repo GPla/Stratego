0b1aeea01c114a56907c9f15b4894ce04dc91b69
GPla <g-platz@hotmail.de>
2019-11-23 00:56:48 +0100

    added unit to pixel

--- core/src/com/mo/stratego/util/Constants.kt
+++ core/src/com/mo/stratego/util/Constants.kt
@@ -1,8 +1,24 @@
 package com.mo.stratego.util
 
+/**
+ * Class that contains game constants.
+ */
 object Constants {
-    // scale: 32 pixels equals 1 game unit
-    val UNITSCALE: Float = 1 / 64f
+    // scale: 64 pixels equals 1 game unit
+    const val TILESCALE: Float = 64f
+    const val UNITSCALE: Float = 1 / TILESCALE
 
+    /**
+     *  Calculates the world unit to a given pixel value.
+     * @param pixel Float
+     * @return The pixel value as game unit.
+     */
     fun getPixelToUnit(pixel: Float) = pixel * UNITSCALE
+
+    /**
+     * Calculates the pixel value to a given world unit.
+     * @param unit Float
+     * @return The world unit as pixel value.
+     */
+    fun getUnitToPixel(unit: Float) = unit * TILESCALE
 }

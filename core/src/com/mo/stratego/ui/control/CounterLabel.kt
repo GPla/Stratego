7114a127457f339907b88055d9fdec1f9a599e71
GPla <g-platz@hotmail.de>
2019-11-23 00:58:27 +0100

    Create CounterLabel.kt

---
+++ core/src/com/mo/stratego/ui/control/CounterLabel.kt
@@ -0,0 +1,32 @@
+package com.mo.stratego.ui.control
+
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.mo.stratego.model.Rank
+import com.mo.stratego.model.map.Grid
+import com.mo.stratego.util.Constants
+
+/**
+ * Custom label class, that display the count of stacked pieces.
+ */
+class CounterLabel(val rank: Rank, val ownerId: Int, style: Skin)
+    : Label("0x", style) {
+
+    init {
+        val position = rank.getDefaultPosition(ownerId)
+        setPosition(Constants.getUnitToPixel(position.x + 0.7f),
+                    Constants.getUnitToPixel(position.y + 0.7f))
+        setScale(1.2f)
+        setColor(1f, 1f, 1f, 1f)
+    }
+
+    var counter: Int = 0
+
+    override fun act(delta: Float) {
+        counter = Grid.spawnMap.getValue(rank)[ownerId]
+        isVisible = counter > 0
+        setText("${counter}x")
+        super.act(delta)
+    }
+
+}

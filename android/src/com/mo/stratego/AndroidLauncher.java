3a82d8a3660fa156289a49fb9643cb042f9c90ba
GPla <36087062+GPla@users.noreply.github.com>
2019-11-26 22:52:00 +0100

    added bluetooth library

--- android/src/com/mo/stratego/AndroidLauncher.java
+++ android/src/com/mo/stratego/AndroidLauncher.java
@@ -6,14 +6,14 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		//disable not needed features to save battery
-		config.useCompass = false;
-		config.useAccelerometer = false;
-		// run game
-		initialize(new StrategoGame(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        //disable not needed features to save battery
+        config.useCompass = false;
+        config.useAccelerometer = false;
+        // run game
+        initialize(new StrategoGame(), config);
+    }
 }

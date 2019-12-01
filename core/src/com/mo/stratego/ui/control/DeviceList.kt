92114344e831bcec812977d42bc2735a0895191b
GPla <36087062+GPla@users.noreply.github.com>
2019-12-01 11:10:31 +0100

    Update DeviceList.kt

--- core/src/com/mo/stratego/ui/control/DeviceList.kt
+++ core/src/com/mo/stratego/ui/control/DeviceList.kt
@@ -3,13 +3,13 @@ package com.mo.stratego.ui.control
 import com.badlogic.gdx.scenes.scene2d.ui.List
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.utils.Array
-import com.mo.stratego.StrategoGame
+import com.mo.stratego.model.communication.CommunicationHandler
 
 class DeviceList(skin: Skin) : List<String>(skin) {
 
     override fun act(delta: Float) {
         // show available devices
         setItems(Array<String>(
-                StrategoGame.comHandler.getAvailableDevices().toTypedArray()))
+                CommunicationHandler.iCom.getAvailableDevices().toTypedArray()))
     }
 }

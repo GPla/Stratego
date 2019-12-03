bd45255059f38cc86254ca4d36526d2c3fc270c8
GPla <36087062+GPla@users.noreply.github.com>
2019-12-03 14:45:09 +0100

    added init player states

--- core/src/com/mo/stratego/model/GameState.kt
+++ core/src/com/mo/stratego/model/GameState.kt
@@ -6,7 +6,9 @@ import com.mo.stratego.model.player.PlayerId
  * Enum of game states
  */
 enum class GameState(val activePlayerId: PlayerId?, val title: String?) {
-    INIT(null, null),
+    INIT(null, "Preparation"),
+    INIT_PLAYER_1(PlayerId.PLAYER1, "Init Player 1"),
+    INIT_PLAYER_2(PlayerId.PLAYER2, "Init Player 2"),
     INIT_PREP_PLAYER_1(PlayerId.PLAYER1, null),
     PREPARATION_PLAYER_1(PlayerId.PLAYER1, "Preparation Player 1"),
     INIT_PREP_PLAYER_2(PlayerId.PLAYER2, null),
@@ -22,7 +24,9 @@ enum class GameState(val activePlayerId: PlayerId?, val title: String?) {
      */
     operator fun inc(): GameState {
         return when (this) {
-            INIT                 -> INIT_PREP_PLAYER_1
+            INIT                 -> INIT_PLAYER_1
+            INIT_PLAYER_1        -> INIT_PLAYER_2
+            INIT_PLAYER_2        -> INIT_PREP_PLAYER_1
             INIT_PREP_PLAYER_1   -> PREPARATION_PLAYER_1
             PREPARATION_PLAYER_1 -> INIT_PREP_PLAYER_2
             INIT_PREP_PLAYER_2   -> PREPARATION_PLAYER_2

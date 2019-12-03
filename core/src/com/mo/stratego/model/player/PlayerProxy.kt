10341d3b68c0f0f37d6ba7e42b43e7ee2ffc8b84
GPla <36087062+GPla@users.noreply.github.com>
2019-12-03 20:14:01 +0100

    added serilization

--- core/src/com/mo/stratego/model/player/PlayerProxy.kt
+++ core/src/com/mo/stratego/model/player/PlayerProxy.kt
@@ -3,6 +3,7 @@ package com.mo.stratego.model.player
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.math.GridPoint2
 import com.mo.stratego.StrategoGame
+import com.mo.stratego.model.Move
 import com.mo.stratego.model.MoveType
 import com.mo.stratego.model.Piece
 import com.mo.stratego.model.PieceFactory
@@ -17,12 +18,30 @@ import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 
 //TODO desc
+//TODO exchange othersmove
 class PlayerProxy(id: PlayerId) : Player(id) {
     init {
         StrategoGame.register(this)
     }
 
-    //TODO exchange othersmove
+    /**
+     * Send move via [CommunicationHandler].
+     */
+    // FIXME: move executed on the wrong side 
+    override var othersMove: Move? = null
+        set(value) {
+            value?.also {
+                // TODO put somewhere else, prettify
+                val mov = Move(it.position,
+                               GridPoint2(it.move.x, 10 - it.move.y))
+                CommunicationHandler.serialize(it)
+            }
+            field = value
+        }
+
+    override var allow: Boolean = false
+        get() = false
+
     private val json = Json(JsonConfiguration.Stable)
 
     override fun presentGrid(pieces: List<Piece>) {
@@ -43,9 +62,12 @@ class PlayerProxy(id: PlayerId) : Player(id) {
         }
     }
 
-    //TODO impl
+    /**
+     * Send [StartingGrid] to via [CommunicationHandler].
+     * @param otherGrid
+     */
     override fun processOthersGrid(otherGrid: StartingGrid) {
-        // TODO exchange via bluetooth
+        CommunicationHandler.serialize(otherGrid)
     }
 
     /**
@@ -53,20 +75,21 @@ class PlayerProxy(id: PlayerId) : Player(id) {
      * @param number Other players starting number
      */
     override fun processOthersStartingNumber(number: StartNumber) {
-        val json = Json(JsonConfiguration.Stable)
-        val jsonData = json.stringify(StartNumber.serializer(), number) + '\n'
-        Gdx.app.log("bth", "json send : $jsonData")
-        CommunicationHandler.iCom.writeData(jsonData.toByteArray())
+        CommunicationHandler.serialize(number)
     }
 
+    //TODO desc 
     @Subscribe(threadMode = ThreadMode.ASYNC)
     fun onDataReceived(event: DataReceivedEvent) {
         val data = event.data?.toString(Charsets.UTF_8) ?: return
         Gdx.app.log("bth", "proxy rec: $data")
 
-
-        startNumber = json.parse(StartNumber.serializer(), data)
-
+        val obj = CommunicationHandler.deserialize(data)
+        when (obj) {
+            is StartNumber  -> startNumber = obj
+            is Move         -> move = obj
+            is StartingGrid -> startingGrid = obj
+        }
 
     }
 

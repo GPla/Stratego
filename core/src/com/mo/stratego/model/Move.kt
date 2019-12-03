0c93a032d8923d4097c1c6b4defeb22621dfb505
GPla <36087062+GPla@users.noreply.github.com>
2019-12-03 14:44:52 +0100

    added desc

--- core/src/com/mo/stratego/model/Move.kt
+++ core/src/com/mo/stratego/model/Move.kt
@@ -2,5 +2,10 @@ package com.mo.stratego.model
 
 import com.badlogic.gdx.math.GridPoint2
 
-//TODO desc
+/**
+ * Class that represents a move on the playing field.
+ *
+ * @property position Position of the [Piece]
+ * @property move Move of the [Piece]
+ */
 class Move(val position: GridPoint2, val move: GridPoint2)

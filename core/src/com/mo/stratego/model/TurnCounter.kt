493fcd69e5896352a2977ab5fe66105c75140bfc
GPla <36087062+GPla@users.noreply.github.com>
2019-12-14 18:58:52 +0100

    added labels

---
+++ core/src/com/mo/stratego/model/TurnCounter.kt
@@ -0,0 +1,26 @@
+package com.mo.stratego.model
+
+/**
+ * Class that keeps track of the current turn.
+ */
+class TurnCounter {
+    /**
+     * Current turn.
+     */
+    var counter : Int = 0
+        private set
+
+    /**
+     * Player who starts the game.
+     */
+    var firstTurn : GameState = GameState.TURN_PLAYER_1
+
+    /**
+     * Updates the turn counter in regard to the current state
+     * @param state Current state
+     */
+    fun changedState(state : GameState){
+        if(state == firstTurn)
+            ++counter
+    }
+}
---
+++ core/src/com/mo/stratego/ui/control/TimerLabel.kt
@@ -0,0 +1,33 @@
+package com.mo.stratego.ui.control
+
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+
+/**
+ * Label, that shows the time since rendered first.
+ */
+class TimerLabel(skin: Skin) : Label("Time: 00:00", skin) {
+    private var lastTick: Float = 0f
+    var seconds: Int = 0
+    var minutes: Int = 0
+
+
+    override fun act(delta: Float) {
+        // delta is in seconds
+        lastTick += delta
+
+        // count up every second
+        if (lastTick >= 1f) {
+            seconds++
+            lastTick = 0f
+            if (seconds >= 60) {
+                minutes++
+                seconds = 0
+            }
+        }
+
+        // update text
+        setText("Time: ${minutes.toString().padStart(2, '0')}" +
+                ":${seconds.toString().padStart(2, '0')}")
+    }
+}

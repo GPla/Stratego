45e569e5f921b38cd712e182af00b33c1b717559
GPla <36087062+GPla@users.noreply.github.com>
2019-12-17 18:09:39 +0100

    added error "handling"

--- core/src/com/mo/stratego/ui/control/DeviceList.kt
+++ core/src/com/mo/stratego/ui/control/DeviceList.kt
@@ -1,15 +1,20 @@
 package com.mo.stratego.ui.control
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.scenes.scene2d.ui.List
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.utils.Array
 import com.mo.stratego.model.communication.CommunicationHandler
 
+//TODO desc
 class DeviceList(skin: Skin) : List<String>(skin) {
-
     override fun act(delta: Float) {
-        // show available devices
-        setItems(Array<String>(
-                CommunicationHandler.iCom.getAvailableDevices().toTypedArray()))
+        try {
+            // show available devices
+            setItems(Array<String>(
+                    CommunicationHandler.iCom.getAvailableDevices().toTypedArray()))
+        } catch (e: Exception) {
+            Gdx.app.log("Error", e.message)
+        }
     }
 }

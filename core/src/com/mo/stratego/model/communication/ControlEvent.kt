573b13b5aa5aa7c52d2d554db1a6c66771fed208
GPla <36087062+GPla@users.noreply.github.com>
2019-12-26 12:38:46 +0100

    added controlevent for game controller

---
+++ core/src/com/mo/stratego/model/communication/ControlEvent.kt
@@ -0,0 +1,33 @@
+package com.mo.stratego.model.communication
+
+import com.mo.stratego.model.GameController
+import kotlinx.serialization.*
+import kotlinx.serialization.internal.StringDescriptor
+
+/**
+ * Enum of control events. For communication between
+ * the [GameController]s.
+ */
+enum class ControlEvent {
+    SURRENDER;
+
+    /**
+     * Serializer for [ControlEvent].
+     */
+    @Serializer(forClass = ControlEvent::class)
+    companion object : KSerializer<ControlEvent> {
+        override val descriptor: SerialDescriptor
+            get() = StringDescriptor
+
+        override fun deserialize(decoder: Decoder): ControlEvent {
+            return valueOf(decoder.decodeString().toUpperCase())
+        }
+
+        override fun serialize(encoder: Encoder, obj: ControlEvent) {
+            encoder.encodeString(obj.name.toLowerCase())
+        }
+
+    }
+
+    private val className = ControlEvent::class.java.name
+}
---
+++ core/src/com/mo/stratego/model/communication/ControlMessage.kt
@@ -0,0 +1,12 @@
+package com.mo.stratego.model.communication
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Wrapper for [ControlEvent]. Enum cannot directly be serialized with KotlinX.
+ * @property event Event
+ */
+@Serializable
+class ControlMessage(val event: ControlEvent) {
+    private val className = ControlMessage::class.java.name
+}

b0fa108bc96c9e216a85fbc795ca0ef5d1127df2
GPla <36087062+GPla@users.noreply.github.com>
2019-12-26 21:51:47 +0100

    added confirm dialog

---
+++ core/src/com/mo/stratego/ui/control/ConfirmDialog.kt
@@ -0,0 +1,27 @@
+package com.mo.stratego.ui.control
+
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+
+class ConfirmDialog(msg: String, val callback: () -> Unit, skin: Skin) :
+    Dialog("Confirmation", skin) {
+    init {
+        isModal = true
+        isMovable = false
+        isResizable = false
+
+        with(contentTable) {
+
+            add(msg)
+            row()
+            button("Confirm", 1)
+            button("Cancel", 0)
+        }
+    }
+
+    override fun result(`object`: Any?) {
+        when (`object` as Int) {
+            1 -> callback()
+        }
+    }
+}
--- core/src/com/mo/stratego/ui/provider/DialogProvider.kt
+++ core/src/com/mo/stratego/ui/provider/DialogProvider.kt
@@ -1,9 +1,11 @@
 package com.mo.stratego.ui.provider
 
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.mo.stratego.StrategoGame
 import com.mo.stratego.model.Atlas
 import com.mo.stratego.ui.Screens
+import com.mo.stratego.ui.control.ConfirmDialog
 import com.mo.stratego.ui.control.GameMenuDialog
 import com.mo.stratego.ui.control.OneButtonDialog
 
@@ -17,6 +19,7 @@ object DialogProvider {
     /**
      * Shows the connection lost dialog. Upon accepting, the main
      * menu is shown.
+     * @param stage Stage
      */
     fun showConnectionLost(stage: Stage) {
         OneButtonDialog("Connection lost",
@@ -28,7 +31,24 @@ object DialogProvider {
         }
     }
 
+    /**
+     * Shows the game menu.
+     * @param stage Stage
+     */
     fun showGameMenu(stage: Stage) {
         gameMenu.show(stage)
     }
+
+    /**
+     * Show confirmation dialog with two options: confirm and cancel.
+     * @param msg Message
+     * @param callback Callback, called if confirm is pressed.
+     * @param stage Stage
+     */
+    fun showConfirmationDialog(msg: String, callback: () -> Unit, skin: Skin,
+                               stage: Stage) {
+
+        ConfirmDialog(msg, callback, skin).show(stage)
+    }
+
 }

e501ec2bb3a27d572e090cdcb14ab9b7d8278b78
GPla <36087062+GPla@users.noreply.github.com>
2019-12-30 00:21:53 +0100

    Create MoveTypeComparator.kt

---
+++ core/src/com/mo/stratego/model/comparator/MoveTypeComparator.kt
@@ -0,0 +1,24 @@
+package com.mo.stratego.model.comparator
+
+import com.badlogic.ashley.core.ComponentMapper
+import com.badlogic.ashley.core.Entity
+import com.mo.stratego.model.MoveType
+import com.mo.stratego.model.component.MoveComponent
+
+/**
+ * Compares two entities with [MoveComponent]s.
+ */
+class MoveTypeComparator : Comparator<Entity> {
+
+    private val mm = ComponentMapper.getFor(MoveComponent::class.java)
+
+    /**
+     * Compares two entities with [MoveComponent]s.
+     * @param p0 Entity
+     * @param p1 Entiy
+     * @return Result of compareTo of [MoveType].
+     */
+    override fun compare(p0: Entity?, p1: Entity?): Int {
+        return mm.get(p0).type.compareTo(mm.get(p1).type)
+    }
+}

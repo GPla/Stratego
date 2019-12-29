423a1f707cda82aae790364a24223a6068f123a8
GPla <36087062+GPla@users.noreply.github.com>
2019-12-29 18:44:19 +0100

    Update MarkdownParser.kt

--- core/src/com/mo/stratego/util/MarkdownParser.kt
+++ core/src/com/mo/stratego/util/MarkdownParser.kt
@@ -10,14 +10,16 @@ import com.badlogic.gdx.utils.Align
 import com.mo.stratego.ui.Atlas
 
 /**
- * TODO
+ * Class to parse an markdown (.md) file to a scene2d representation.
  */
 object MarkdownParser {
     private val allowExtensions = arrayOf("md", "txt")
     private val sWidth: Float = Constants.screenWidth - 40f
+    private val lineBreaks = arrayOf("\n", "\r", "\r\n", "")
 
     /**
      * Parses a markdown document to scene2D ui elements.
+     * It can parse 3 level of headers, images and text.
      * @param file File
      * @return Scene2D table of markdown content.
      */
@@ -27,53 +29,71 @@ object MarkdownParser {
 
         val resultTable = Table()
         resultTable.align(Align.top)
+        resultTable.width = Constants.screenWidth
 
+        // parent folder
+        val folder = file.parent().path()
+
+        // block of text, will be divided into sections by headers and images
         var contentBlock: Label? = null
-        val content = file.readString().split('\n')
+        val content = file.readString("UTF-8").split('\n')
         for (line in content) {
             when {
-                line.startsWith("# ")                               -> {
-                    // header
+                // image header
+                line.matches("""#+.*!\[[^\s]*\]\([^\s]+\.png\)\s*""".toRegex())
+                -> {
                     contentBlock = null
-                    val lbl = processHeader(line.substring(2), 0)
+                    // use inner table to avoid multiple columns in result table
+                    val innerTable = Table()
+                    with(innerTable) {
+                        add(processHeader(line.substringBefore("!")))
+                                .pad(10f, 0f, 0f, 10f)
+                        add(processImage(line.substringAfter("!"), folder))
+                    }
+
                     with(resultTable) {
                         row()
-                        add(lbl).expandX().pad(30f, 50f, 0f, 10f)
-                                .align(Align.left)
+                        add(innerTable).align(Align.left)
                     }
                 }
 
-                line.matches("""!\[\w*\]\(\w*.png\)\s""".toRegex()) -> {
-                    // image
+                // header
+                line.startsWith("#") -> {
                     contentBlock = null
-                    val path = line.substring(line.indexOf('(') + 1,
-                                              line.indexOf(')'))
-                    val tex = Texture(Gdx.files.internal(
-                            file.parent().path() + "/" + path))
+                    val lbl = processHeader(line)
                     with(resultTable) {
                         row()
-                        add(Image(tex)).width(sWidth)
-                                .height(scaleImage(tex, sWidth))
-
+                        add(lbl).pad(10f, 0f, 0f, 20f).align(Align.left)
                     }
                 }
 
-                line.equals('\n')                                   -> {
-                    // ignore
+                // image
+                line.matches("""!\[[^\s]*\]\([^\s]+\.png\)\s*""".toRegex()) -> {
+                    contentBlock = null
+
+                    val img = processImage(line, folder)
+
+                    with(resultTable) {
+                        row()
+                        add(img).width(img.width).height(img.height)
+                                .padBottom(10f)
+                    }
                 }
 
-                else                                                -> {
-                    // text
+                // text
+                line !in lineBreaks -> {
+                    // start new section
                     if (contentBlock == null) {
-                        contentBlock = Label("", Atlas.uiSkin)
+                        contentBlock = Label(line, Atlas.uiSkin)
                         contentBlock.setWrap(true)
 
                         with(resultTable) {
                             row()
                             add(contentBlock).pad(20f).width(sWidth)
                         }
+                    } else {
+                        contentBlock.text.appendLine("$line")
                     }
-                    contentBlock.text.append("$line\n")
                 }
             }
         }
@@ -82,16 +102,15 @@ object MarkdownParser {
     }
 
     /**
-     * TODO
-     *
-     * @param text
-     * @param level
-     * @return
+     * @param line Line
+     * @return Label of the text with the appropriate font size.
      */
-    private fun processHeader(text: String, level: Int): Label {
+    private fun processHeader(line: String): Label {
+        val level = line.lastIndexOf('#') // header level
+        val text = line.substring(level + 1) // extract text
         val font = when (level) {
-            0    -> Atlas.font48
-            1    -> Atlas.font32
+            0 -> Atlas.font48
+            1 -> Atlas.font32
             else -> Atlas.font26
         }
         val style = Label.LabelStyle(font, Constants.YELLOW)
@@ -99,14 +118,34 @@ object MarkdownParser {
     }
 
     /**
-     * TODO
-     *
-     * @param texture
-     * @param width
-     * @return
+     * Processes an image and scales it to fit the screen.
+     * @param line Line
+     * @param prefix Path prefix
+     * @return The loaded image.
      */
-    private fun scaleImage(texture: Texture, width: Float): Float {
-        val scale = texture.width / texture.height.toFloat()
-        return width * scale
+    private fun processImage(line: String, prefix: String): Image {
+        // extract path
+        val path = line.substring(line.indexOf('(') + 1, line.indexOf(')'))
+
+        // load texture
+        val texture = Texture(Gdx.files.internal("$prefix/$path"))
+
+        return scaleImage(Image(texture))
+    }
+
+    /**
+     * Scales the image dimensions, if the image exceeds the screen width,
+     * in same aspect ratio as the original.
+     * @param img Image
+     * @return Image for chaining.
+     */
+    private fun scaleImage(img: Image): Image {
+        if (img.width < sWidth)
+            return img
+
+        val scale = img.height / img.width
+        img.width = if (img.width >= sWidth) sWidth else img.width
+        img.height = img.width * scale
+        return img
     }
 }

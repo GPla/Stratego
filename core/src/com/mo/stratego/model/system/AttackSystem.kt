5fd4e22146910fefd474973d17c6934e218f19d7
GPla <36087062+GPla@users.noreply.github.com>
2019-12-30 00:24:13 +0100

    added sound

--- core/src/com/mo/stratego/model/system/AttackSystem.kt
+++ core/src/com/mo/stratego/model/system/AttackSystem.kt
@@ -5,13 +5,16 @@ import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.Family
 import com.badlogic.ashley.systems.IteratingSystem
 import com.badlogic.gdx.Gdx
+import com.mo.stratego.model.Rank
 import com.mo.stratego.model.Result
 import com.mo.stratego.model.component.AttackComponent
 import com.mo.stratego.model.component.PieceComponent
+import com.mo.stratego.model.component.SoundComponent
 import com.mo.stratego.model.component.WaitComponent
 import com.mo.stratego.model.game.GameController
 import com.mo.stratego.model.game.GameResult
 import com.mo.stratego.model.player.PlayerId
+import com.mo.stratego.model.sound.SoundType
 import com.mo.stratego.util.Constants
 
 /**
@@ -39,7 +42,6 @@ class AttackSystem :
         val enemy = attackMapper.get(entity)?.piece ?: return
 
         val result = piece.rank.attacks(enemy.rank)
-
         Gdx.app.log(Constants.TAG_GAME, "result: $result")
 
         // process attack
@@ -50,25 +52,49 @@ class AttackSystem :
                     PlayerId.PLAYER2 -> GameController.end(GameResult.LOST)
                 }
             }
-            Result.WON      -> {
+            Result.WON -> {
                 // attacking piece won
                 // move enemy piece to graveyard
                 piece.showDefault()
                 enemy.returnToDefaultPosition()
             }
-            Result.LOST     -> {
+            Result.LOST -> {
                 // attacking piece lost
                 // move to graveyard
                 enemy.showDefault()
                 piece.returnToDefaultPosition()
             }
-            Result.DRAW     -> {
+            Result.DRAW -> {
                 // both pieces are dead
                 // move both to graveyard
                 enemy.returnToDefaultPosition()
                 piece.returnToDefaultPosition()
             }
         }
+
+        // sound
+        val sound: SoundType? = when (result) {
+            Result.WON -> {
+                if (piece.rank == Rank.SPY && enemy.rank == Rank.MARSHAL)
+                    SoundType.SPY_CAPTURE
+                else if (piece.rank == Rank.MINER && enemy.rank == Rank.BOMB)
+                    SoundType.DEFUSE_BOMB
+                else
+                    SoundType.BATTLE_WON
+            }
+            Result.LOST, Result.DRAW -> {
+                if (enemy.rank == Rank.BOMB)
+                    SoundType.EXPLOSION
+                else
+                    SoundType.BATTLE_LOST
+            }
+            else -> null
+        }
+
+        sound?.also {
+            piece.add(SoundComponent(sound))
+        }
+
 
         entity.remove(AttackComponent::class.java)
     }

8c3eacbb7966e6f0c22183aeaebcf3ec062d084a
GPla <36087062+GPla@users.noreply.github.com>
2019-12-30 00:24:26 +0100

    added sound and music

--- core/src/com/mo/stratego/model/sound/SoundProvider.kt
+++ core/src/com/mo/stratego/model/sound/SoundProvider.kt
@@ -1,6 +1,10 @@
 package com.mo.stratego.model.sound
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.audio.Music
+import com.badlogic.gdx.audio.Sound
 import com.mo.stratego.StrategoGame
+import com.mo.stratego.util.Constants
 
 /**
  * Provider class for music and sound effects.
@@ -9,6 +13,33 @@ object SoundProvider {
     // preference key
     private const val MUSIC_KEY = "Music"
     private const val DEFAULT_MUSIC = true
+    const val SOUND_PATH = "sounds"
+
+    /**
+     * Map of [SoundType] to [Sound].
+     */
+    private val soundMap = mutableMapOf<SoundType, Sound>()
+
+    /**
+     * Currently playing music.
+     */
+    private val playingMusic = mutableListOf<Music>()
+
+    /**
+     * Initialize provider.
+     */
+    fun init() {
+        loadSounds()
+    }
+
+    /**
+     * Loads all sounds defined in [SoundType].
+     */
+    private fun loadSounds() {
+        SoundType.values().forEach {
+            soundMap[it] = Gdx.audio.newSound(Gdx.files.internal(it.path))
+        }
+    }
 
     /**
      * Whether or not sounds are played. Value is saved and retrieved from
@@ -27,5 +58,42 @@ object SoundProvider {
             }
         }
 
+    /**
+     * Plays a sound.
+     * @param soundType Sound to play
+     * @return Sound handle.
+     */
+    fun playSound(soundType: SoundType): Long? {
+        if (!isTurnedOn)
+            return null
+        Gdx.app.log(Constants.TAG_SOUND, "sound played: $soundType")
+        return soundMap[soundType]?.play()
+    }
 
+
+    /**
+     * Plays music in a loop.
+     * @param musicType Music to play
+     * @return Music handle.
+     */
+    fun playMusic(musicType: MusicType): Int? {
+        if (!isTurnedOn)
+            return null
+
+        Gdx.app.log(Constants.TAG_SOUND, "music played: $musicType")
+        val music = Gdx.audio.newMusic(Gdx.files.internal(musicType.path))
+        music.isLooping = true
+        music.play()
+        playingMusic.add(music)
+        return playingMusic.indexOf(music)
+    }
+
+    /**
+     * Stops music.
+     * @param musicHandle Music handle from PlayMusic
+     */
+    fun stopMusic(musicHandle: Int = 0) {
+        if (musicHandle in playingMusic.indices)
+            playingMusic[musicHandle].stop()
+    }
 }

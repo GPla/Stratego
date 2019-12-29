bcd54dced9be1406a15b6651702c636b434d45cb
GPla <36087062+GPla@users.noreply.github.com>
2019-12-30 00:22:06 +0100

    Create SoundComponent.kt

---
+++ core/src/com/mo/stratego/model/component/SoundComponent.kt
@@ -0,0 +1,6 @@
+package com.mo.stratego.model.component
+
+import com.badlogic.ashley.core.Component
+import com.mo.stratego.model.sound.SoundType
+
+class SoundComponent(val soundType: SoundType) : Component

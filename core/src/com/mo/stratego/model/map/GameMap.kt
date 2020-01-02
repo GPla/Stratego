3aafaef9faa25eb8f3338a12065b484cd40d31ef
GPla <36087062+GPla@users.noreply.github.com>
2020-01-02 05:18:46 +0100

    fixed statics

--- core/src/com/mo/stratego/model/map/GameMap.kt
+++ core/src/com/mo/stratego/model/map/GameMap.kt
@@ -5,28 +5,33 @@ import com.badlogic.gdx.maps.tiled.TiledMap
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer
 import com.badlogic.gdx.maps.tiled.TmxMapLoader
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+import com.badlogic.gdx.utils.Disposable
 import com.mo.stratego.util.Constants
 
 /**
  * Singleton Class that loads and renders the Map
  */
-object GameMap {
-    private const val path: String = "maps/map2.tmx"
-
-    private var map: TiledMap
-    private val mapRenderer: TiledMapRenderer
+object GameMap : Disposable {
+    private lateinit var map: TiledMap
+    private lateinit var mapRenderer: TiledMapRenderer
 
     //map properties
-    val width: Int      // width of loaded map
-    val height: Int     // height of loaded map
-    val gridBottom: Int // number of tiles from bottom to grid
-    val gridLeft: Int   // number of tiles from left to grid
-    val tilewidth: Int
-    val tileheight: Int
+    var width: Int = 0    // width of loaded map
+        private set
+    var height: Int = 0  // height of loaded map
+        private set
+    var gridBottom: Int = 0 // number of tiles from bottom to grid
+        private set
+    var gridLeft: Int = 0  // number of tiles from left to grid
+        private set
+    var tilewidth: Int = 0
+        private set
+    var tileheight: Int = 0
+        private set
 
-    init {
+    fun init() {
         // load map and init renderer
-        map = TmxMapLoader().load(path)
+        map = TmxMapLoader().load(Constants.MAP_PATH)
         mapRenderer = OrthogonalTiledMapRenderer(map, Constants.UNITSCALE)
 
         // get properties of map
@@ -37,6 +42,7 @@ object GameMap {
         width = scale(props.get("width", Int::class.java), tilewidth)
         height = scale(props.get("height", Int::class.java), tileheight)
 
+        // user defined properties
         gridBottom = if (props.containsKey("grid_bottom"))
             scale(props.get("grid_bottom", Int::class.java), tileheight) else 0
         gridLeft = if (props.containsKey("grid_left"))
@@ -60,4 +66,10 @@ object GameMap {
         mapRenderer.render()
     }
 
+    /**
+     * Disposes loaded resources.
+     */
+    override fun dispose() {
+        map.dispose()
+    }
 }
